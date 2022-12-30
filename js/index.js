081be7f0c0e9ae0cab70fd93b9cad923f35520b4
hamidmk <shrmk59@gmail.com>
2022-12-30 11:23:49 +0330

    test1

---
+++ js/index.js
@@ -0,0 +1,8 @@
+var MyName = "حمید رضا کاشانی";
+// alert("سلام " + MyName);
+function SumNum() {
+    //print on html page when clicking on button
+  var NumOne = 10;
+  var NumTwo = 5;
+  document.getElementById("print_sum").innerHTML= MyName + ' ' + NumOne * NumTwo;
+}
